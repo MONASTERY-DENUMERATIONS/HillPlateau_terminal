442a274a3acdc7eaaced9b4e98588652e4d61cdd
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-06-21 23:22:11 +0530

    Add initial structure for Name.js with data fields

---
+++ Name.js
@@ -0,0 +1,28 @@
+Entered country : "country that you migrated to",
+Native-country : "country that came from",
+Born_year : "Year of Birth",
+
+ROL : 'Reason of Leaving' : [
+
+
+       case: education[
+          collect--college:centre
+       ]
+       case: work[
+         collect--place:jobid:job-role
+       ]
+  
+]
+Free_collect, Name, RCC : [ticket-if: 'wrong-paint' , 'leave' = 'time' , return = wait()]
+Centre.work()
+work:place:settings : {collect-[ethical_backgrounds,IO-stat[country-web, Gx-gcc]]}
+Compiler: [c.bear(offset:out-load[
+        connect-pipline: Abroad,
+        remaining_last : Siblings,
+  Rotor-form[wheel, transport : safe, living_space]
+  Permanent_Residency : On-call : <Data.view, past_history(social)>                 
+])]
+
+Spin-off: wheel , 
+Collector: Forum -> Bus.scale('initial', 'custom' ,'array-mentioned' ,'early-arrival')
+Connection : All_terminal : [Regular-stand,local()]
