c7054220f96a2ef8a62ee63f55f461a99df8f3e3
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-07-16 18:42:29 +0530

    Enhance Main-Entrance with new functions
    Added County_Estate and Trailer_Src functions to Main-Entrance.cpp.

--- Main-Entrance.cpp
+++ Main-Entrance.cpp
@@ -17,17 +17,20 @@
            Entry_State : <Regional.[self , innit = 'begin']> ;
            Action_ground : <Rural>[Entry_level = 'Urban'] ;
            Transit-stage : <AGE> , Background() ;
+           County_Estate : Estate_Nials[-beaver:side , Mine_Fountain]
+           Trailer_Src() : Src: Ground |FEEDER|GATER|FRAME|WAY|SPORTS|COMPLEX|WAYS|NUMBER_GRAD|LETTER_PATIENT
   }
 
 exit entry : {
   const state()=> {change , Refferal() , Bypass: [
     Entry : State[Prods] => change(name) : [Project = 'onset' , 'evolving']
     H-state : Background(Story)=> [Major changes  in ruleset , Main-set -> Manifesto-[Explain.via(sentry)]]
+    Off-store : On-store(History , no-seperations[online,offline : [Base-set : /End-set]])
   ]}
 
   ROUTING_SET(Ctrl M) :[Mset , arrival() , [Attended , Backed() , Server-relations , Distributed_data_sections , Arrearfolds , min(ton)]]
-  
-
+  If-formed()=> [BAN:NO:WAY : <Trivial-esc[note--creepy{ref.prof/}]>]
+  [/mean-set: E-state, Column{Arrival, Set-on : <Rc.net :  [Venn-prag, [Arc-intersections][Dop_int = > instantiate()]]>}]
   H-gram : <Regional> , [Exposure ,  Conduct] : [Lessons() : Legions() : NameError() , Name_Travels()] = > [
 
     Arrived_Seq_column {Base , env.prod[J:Sections]}
