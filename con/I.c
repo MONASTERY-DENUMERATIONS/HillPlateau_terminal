fc035550f87686fc32c9ed2c5d83b87d00990a32
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-07-27 00:55:51 +0530

    Create I.c

---
+++ con/I.c
@@ -0,0 +1,18 @@
+Malfunction(IC) ,  CI : <
+  
+  Iqui-don't servidang ,  ovrikond ,  dornegae , ivrkond : <Dovesnikkon , Sovrikund , :  Bednina>
+  HAvrikon't : <Degine ,  dondono ,  devpire : -  h  []  , Stiver(P, Quantex(, Stav : lecture(< meant :  texture , deck :  Sima)')) >
+
+  Heyasrk : 'Don't' , Merscki() - : [ + []
+  Souveniscta : 'Sovkii-pond ]
+  Meybaom : 'ArchiDon't' , ;: Deyveekam meyvikonnm 
+  heyski: -  fomr , dordasx: - {}[]Forum , Morvi :  serm : <ARCHVII : [
+  Dwall ()  , [Samber drunk i ,  ivant  kandi , - []Stovereiein : [
+     Quant , Solvidex: [] 
+     Frun () -  cordidiaid : []
+    S aveouir : 'Cunt :  X-  contivala()   : 'Haskkom' , 'Devam()' 
+  ]  ], mei - kal m : 'ZOIPM : [] 
+     MESKSONDIDPI  -  [ ]  MESKOND| FROTOGG  ,  DECNANA; Y : ][ 
+     TENETENEN
+  ] >
+  >
